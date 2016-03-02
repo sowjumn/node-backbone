43a241b549a264b083ef0e65ced61ffedb3faa42
sowjanya <sowju19@gmail.com>
2016-03-01 19:15:05 -0800

    added api

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 var morgan = require('morgan');
 
 //Configuration
 app.use(express.static(__dirname + '/dist'));
 app.use(morgan('dev'));
+app.use('/api', router);
 
 //Routing
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/index.html');
+  
+});
+
+router.route('/sowju').get(function(req, res) {
+  res.json({message: 'awesome'});
 });
 
 app.listen(8080);
